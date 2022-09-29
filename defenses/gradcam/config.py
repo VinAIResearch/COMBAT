2070513ce1c410f6805a41a5a6743628fc57085c
Tran Huynh <111326152+tranhn2@users.noreply.github.com>
2022-09-29 12:16:48 +0700

    Update GradCAM

---
+++ defenses/gradcam/config.py
@@ -0,0 +1,29 @@
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_root', type=str, default='../../data/')   
+    parser.add_argument('--checkpoints', type=str, default='../../checkpoints/')
+    parser.add_argument('--temps', type=str, default='./temps')
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--saving_prefix', type=str, help='Folder in /checkpoints for saving ckpt')
+    parser.add_argument('--results', type=str, default='./results')
+
+    parser.add_argument('--dataset', type=str, default='mnist')
+    parser.add_argument('--input_height', type=int, default=None)
+    parser.add_argument('--input_width', type=int, default=None)
+    parser.add_argument('--input_channel', type=int, default=None)
+    parser.add_argument('--num_classes', type=int, default=10)
+
+    parser.add_argument('--num_workers', type=int, default=2)
+    parser.add_argument('--bs', type=int, default=128)
+    parser.add_argument('--noise_rate', type=float, default=0.08)
+    parser.add_argument('--target_label', type=int, default=0)
+
+    parser.add_argument('--random_rotation', type=int, default=10)
+    parser.add_argument('--random_crop', type=int, default=5)
+    parser.add_argument('--attack_mode', type=str, default='all2one', help='all2one or all2all')
+
+    return parser

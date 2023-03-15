f154e40d116aa2658daa81f29016498b955a5e85
Tran Huynh <111326152+tranhn2@users.noreply.github.com>
2023-03-15 16:22:58 +0700

    Update dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -2,12 +2,15 @@ import csv
 import os
 import random
 
+import config
+
 import kornia.augmentation as A
 import torch
 import torch.utils.data as data
 import torchvision
 import torchvision.transforms as transforms
 from PIL import Image
+import glob
 from torch.utils.tensorboard import SummaryWriter
 
 
@@ -27,6 +30,7 @@ class ProbTransform(torch.nn.Module):
 def get_transform(opt, train=True, pretensor_transform=False):
     transforms_list = []
     transforms_list.append(transforms.Resize((opt.input_height, opt.input_width)))
+    
     if pretensor_transform:
         if train:
             transforms_list.append(transforms.RandomCrop((opt.input_height, opt.input_width), padding=opt.random_crop))
@@ -35,12 +39,8 @@ def get_transform(opt, train=True, pretensor_transform=False):
                 transforms_list.append(transforms.RandomHorizontalFlip(p=0.5))
 
     transforms_list.append(transforms.ToTensor())
-    if opt.dataset == "cifar10":
+    if opt.dataset in ["cifar10", "celeba", "imagenet10"]:
         transforms_list.append(transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]))  # transforms.Normalize([0.4914, 0.4822, 0.4465], [0.247, 0.243, 0.261]))
-    elif opt.dataset == "mnist":
-        transforms_list.append(transforms.Normalize([0.5], [0.5]))
-    elif opt.dataset == "gtsrb" or opt.dataset == "gtsrb2" or opt.dataset == "celeba":
-        transforms_list.append(transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]))  # pass
     else:
         raise Exception("Invalid Dataset")
     return transforms.Compose(transforms_list)
@@ -50,7 +50,7 @@ class PostTensorTransform(torch.nn.Module):
     def __init__(self, opt):
         super(PostTensorTransform, self).__init__()
         if opt.post_transform_option != "no_use":
-            if not (opt.dataset != "gtsrb" and opt.post_transform_option == "use_modified"):
+            if not (opt.post_transform_option == "use_modified"):
                 self.random_crop = ProbTransform(A.RandomCrop((opt.input_height, opt.input_width), padding=opt.random_crop), p=0.8)
             self.random_rotation = ProbTransform(A.RandomRotation(opt.random_rotation), p=0.5)
             if opt.dataset == "cifar10":
@@ -61,100 +61,6 @@ class PostTensorTransform(torch.nn.Module):
             x = module(x)
         return x
 
-
-class GTSRB(data.Dataset):
-    def __init__(self, opt, train, transforms, target_label=None):
-        super(GTSRB, self).__init__()
-        if target_label is not None:
-            assert target_label < opt.num_classes
-        self.num_classes = opt.num_classes
-        if train:
-            self.data_folder = os.path.join(opt.data_root, "GTSRB/Train")
-            self.images, self.labels = self._get_data_train_list(target_label)
-        else:
-            self.data_folder = os.path.join(opt.data_root, "GTSRB/Test")
-            self.images, self.labels = self._get_data_test_list(target_label)
-        self.transforms = transforms
-
-    def _get_data_train_list(self, target_label=None):
-        images = []
-        labels = []
-        l = list(range(0, self.num_classes))
-        if target_label is not None:
-            l = [target_label]
-        for c in l:
-            prefix = self.data_folder + "/" + format(c, "05d") + "/"
-            gtFile = open(prefix + "GT-" + format(c, "05d") + ".csv")
-            gtReader = csv.reader(gtFile, delimiter=";")
-            next(gtReader)
-            for row in gtReader:
-                images.append(prefix + row[0])
-                labels.append(int(row[7]))
-            gtFile.close()
-        return images, labels
-
-    def _get_data_test_list(self, target_label=None):
-        images = []
-        labels = []
-        prefix = os.path.join(self.data_folder, "GT-final_test.csv")
-        gtFile = open(prefix)
-        gtReader = csv.reader(gtFile, delimiter=";")
-        next(gtReader)
-        l = set(range(0, self.num_classes))
-        for row in gtReader:
-            # if target_label is not None:
-            #   if int(row[7]) != target_label:
-            #      continue
-            if int(row[7]) in l:
-                images.append(self.data_folder + "/" + row[0])
-                labels.append(int(row[7]))
-
-        return images, labels
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, index):
-        image = Image.open(self.images[index])
-        image = self.transforms(image)
-        label = self.labels[index]
-        return image, label
-
-
-class GTSRB2(data.Dataset):
-    def __init__(self, opt, train, transforms, target_label=None):
-        super(GTSRB2, self).__init__()
-        self.data_folder = os.path.join(opt.data_root, "GTSRB/Train")
-        self.images, self.labels = self._get_data_train_list(target_label)
-        self.transforms = transforms
-
-    def _get_data_train_list(self, target_label=None):
-        images = []
-        labels = []
-        l = list(range(0, 43))
-        if target_label is not None:
-            l = [target_label]
-        for c in l:
-            prefix = self.data_folder + "/" + format(c, "05d") + "/"
-            gtFile = open(prefix + "GT-" + format(c, "05d") + ".csv")
-            gtReader = csv.reader(gtFile, delimiter=";")
-            next(gtReader)
-            for row in gtReader:
-                res = int(row[0][:-4][-5:])
-                if res > 27:
-                    images.append(prefix + row[0])
-                    labels.append(int(row[7]))
-            gtFile.close()
-        return images, labels
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, index):
-        image = Image.open(self.images[index])
-        image = self.transforms(image)
-        label = self.labels[index]
-        return image, label
 
 
 class CelebA_attr(data.Dataset):
@@ -177,20 +83,26 @@ class CelebA_attr(data.Dataset):
         return (input, target)
 
 
+class ImageNet(data.Dataset):
+    def __init__(self, opt, split, transforms):
+        self.dataset = torchvision.datasets.ImageNet(root=os.path.join(opt.data_root, "imagenet10"), split=split)
+        self.transforms = transforms
+        self.split = split
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        input, target = self.dataset[index]
+        input = self.transforms(input)
+        return (input, target)
+    
+
 def get_dataloader(opt, train=True, pretensor_transform=False, target_label=None, bs=None, shuffle=True):
     if bs is None:
         bs = opt.bs
     transform = get_transform(opt, train, pretensor_transform)
-    if opt.dataset == "gtsrb":
-        dataset = GTSRB(opt, train, transform, target_label=target_label)
-    elif opt.dataset == "gtsrb2":
-        dataset = GTSRB2(opt, train, transform, target_label=target_label)
-    elif opt.dataset == "mnist":
-        dataset = torchvision.datasets.MNIST(opt.data_root, train, transform, download=True)
-        if target_label is not None:
-            pairs = [(x, y) for x, y in zip(dataset.data, dataset.targets) if int(y) == target_label]
-            dataset.data, dataset.targets = [x[0] for x in pairs], [x[1] for x in pairs]
-    elif opt.dataset == "cifar10":
+    if opt.dataset == "cifar10":
         dataset = torchvision.datasets.CIFAR10(opt.data_root, train, transform, download=True)
         if target_label is not None:
             pairs = [(x, y) for x, y in zip(dataset.data, dataset.targets) if int(y) == target_label]
@@ -201,22 +113,18 @@ def get_dataloader(opt, train=True, pretensor_transform=False, target_label=None
         else:
             split = "test"
         dataset = CelebA_attr(opt, split, transform)
+    elif opt.dataset == 'imagenet10':
+        split = 'train' if train else 'val'
+        dataset = ImageNet(opt, split, transform)
     else:
         raise Exception("Invalid dataset")
+    if opt.debug:
+        dataset = torch.utils.data.Subset(dataset, range(min(len(dataset), 1000)))
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=bs, num_workers=opt.num_workers, shuffle=shuffle, pin_memory=True)
     return dataloader
 
 
 def main():
-    # opt = config.get_arguments().parse_args()
-    # transforms = get_transform(opt, False)
-    # dataloader = get_dataloader(opt, False)
-    # for item in dataloader:
-    #     images, labels = item
-    # opt = config.get_arguments().parse_args()
-    # dataset = torchvision.datasets.CelebA(root=opt.data_root, split='test',
-    #                                       target_type='identity', download=True)
-    # sooka = dataset[51439]
     pass
 
 

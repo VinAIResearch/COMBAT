ba65805df6c440dd5755d2a54cdf1b6ffea476e1
Tran Huynh <111326152+tranhn2@users.noreply.github.com>
2023-03-15 16:44:15 +0700

    Update gradcam

--- defenses/gradcam/config.py
+++ defenses/gradcam/config.py
@@ -9,6 +9,7 @@ def get_arguments():
     parser.add_argument("--temps", type=str, default="./temps")
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--saving_prefix", type=str, help="Folder in /checkpoints for saving ckpt")
+    parser.add_argument("--load_checkpoint_clean", type=str)
     parser.add_argument("--results", type=str, default="./results")
 
     parser.add_argument("--dataset", type=str, default="cifar10")
@@ -21,6 +22,9 @@ def get_arguments():
     parser.add_argument("--bs", type=int, default=128)
     parser.add_argument("--noise_rate", type=float, default=0.08)
     parser.add_argument("--target_label", type=int, default=0)
+    parser.add_argument("--ratio", type=float, default=0.65, help="scale ratio for DCT of noise")
+    parser.add_argument("--kernel_size", type=int, default=3, help="kernel size for Gaussian blur")
+    parser.add_argument("--sigma", type=tuple, default=(0.1, 1.0), help="sigma for Gaussian blur")
 
     parser.add_argument("--random_rotation", type=int, default=10)
     parser.add_argument("--random_crop", type=int, default=5)

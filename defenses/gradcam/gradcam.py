47b6c674b2ceef4f2d7db0aadc6ea5f3380afad7
Tran Huynh <111326152+tranhn2@users.noreply.github.com>
2023-03-15 16:45:38 +0700

    Update gradcam

--- defenses/gradcam/gradcam.py
+++ defenses/gradcam/gradcam.py
@@ -1,41 +1,55 @@
 import argparse
-import os
-import sys
-
 import cv2
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import os
+import copy
 import torchvision
-
+import torchvision.transforms as T
+import torch.nn.functional as F
+import torch.nn as nn
 from config import get_arguments
 
-sys.path.insert(0, "../..")
-from PIL import Image, ImageDraw, ImageFont
+import sys
+sys.path.insert(0,'../..')
+from utils.dataloader import get_dataloader, PostTensorTransform
+from utils.utils import progress_bar
+from utils.dct import *
+from classifier_models import PreActResNet18, ResNet18
+from networks.models import AE, Normalizer, Denormalizer, UnetGenerator
 from torch.autograd import Function
 from torchvision import models
-
-from classifier_models import PreActResNet10, PreActResNet18, ResNet18
-from networks.models import Denormalizer, Normalizer, UnetGenerator
-from utils.dataloader import PostTensorTransform, get_dataloader
-from utils.utils import progress_bar
+from PIL import Image, ImageDraw, ImageFont
 
 
+
+def low_freq(x, opt):
+    image_size = opt.input_height
+    ratio = opt.ratio
+    mask = torch.zeros_like(x)
+    mask[:, :, :int(image_size * ratio), :int(image_size * ratio)] = 1
+    x_dct = dct_2d((x+1)/2*255)
+    x_dct *= mask
+    x_idct = (idct_2d(x_dct)/255*2) - 1
+    return x_idct
+
+        
 def text_phantom(text, size):
     # Availability is platform dependent
-    font = "LiberationSans-Regular"
+    font = 'LiberationSans-Regular'
 
     # Create font
-    pil_font = ImageFont.truetype(font + ".ttf", size=size // len(text), encoding="unic")
+    pil_font = ImageFont.truetype(font + ".ttf", size=size // len(text),
+                                  encoding="unic")
     text_width, text_height = pil_font.getsize(text)
 
     # create a blank canvas with extra space between lines
-    canvas = Image.new("RGB", [size, size], (255, 255, 255))
+    canvas = Image.new('RGB', [size, size], (255, 255, 255))
 
     # draw the text onto the canvas
     draw = ImageDraw.Draw(canvas)
-    offset = ((size - text_width) // 2, (size - text_height) // 2)
+    offset = ((size - text_width) // 2,
+              (size - text_height) // 2)
     white = "#000000"
     draw.text(offset, text, font=pil_font, fill=white)
 
@@ -49,47 +63,47 @@ class Normalize:
         self.expected_values = expected_values
         self.variance = variance
         assert self.n_channels == len(self.expected_values)
-
+    
     def __call__(self, x):
         x_clone = x.clone()
         for channel in range(self.n_channels):
             x_clone[:, channel] = (x[:, channel] - self.expected_values[channel]) / self.variance[channel]
         return x_clone
-
-
+    
+    
 class Denormalize:
     def __init__(self, opt, expected_values, variance):
         self.n_channels = opt.input_channel
         self.expected_values = expected_values
         self.variance = variance
         assert self.n_channels == len(self.expected_values)
-
+    
     def __call__(self, x):
         x_clone = x.clone()
         for channel in range(self.n_channels):
             x_clone[:, channel] = x[:, channel] * self.variance[channel] + self.expected_values[channel]
         return x_clone
-
-
+    
+        
 def get_normalize(opt):
-    if opt.dataset == "cifar10" or opt.dataset == "gtrsb":
-        normalizer = Normalize(opt, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-    else:
-        raise Exception("Invalid dataset")
-    return normalizer
-
-
+        if(opt.dataset == 'cifar10' or opt.dataset == 'gtrsb'):
+            normalizer = Normalize(opt, [0.5, 0.5], [0.5, 0.5])
+        else:
+            raise Exception("Invalid dataset")
+        return normalizer
+    
+ 
 def get_denormalize(opt):
-    if opt.dataset == "cifar10" or opt.dataset == "gtsrb":
-        denormalizer = Denormalize(opt, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-    else:
-        raise Exception("Invalid dataset")
-    return denormalizer
-
-
-class FeatureExtractor:
-    """Class for extracting activations and
-    registering gradients from targetted intermediate layers"""
+        if(opt.dataset == 'cifar10' or opt.dataset == 'gtsrb'):
+            denormalizer = Denormalize(opt, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        else: 
+            raise Exception("Invalid dataset")
+        return denormalizer
+    
+        
+class FeatureExtractor():
+    """ Class for extracting activations and 
+    registering gradients from targetted intermediate layers """
 
     def __init__(self, model, target_layers):
         self.model = model
@@ -110,11 +124,11 @@ class FeatureExtractor:
         return outputs, x
 
 
-class ModelOutputs:
-    """Class for making a forward pass, and getting:
+class ModelOutputs():
+    """ Class for making a forward pass, and getting:
     1. The network output.
     2. Activations from intermeddiate targetted layers.
-    3. Gradients from intermeddiate targetted layers."""
+    3. Gradients from intermeddiate targetted layers. """
 
     def __init__(self, model, feature_module, target_layers):
         self.model = model
@@ -131,7 +145,7 @@ class ModelOutputs:
                 target_activations, x = self.feature_extractor(x)
             elif "avgpool" in name.lower():
                 x = module(x)
-                x = x.view(x.size(0), -1)
+                x = x.view(x.size(0),-1)
             else:
                 x = module(x)
             print(x.shape)
@@ -192,6 +206,7 @@ class GradCam:
 
 
 class GuidedBackpropReLU(Function):
+
     @staticmethod
     def forward(self, input):
         positive_mask = (input > 0).type_as(input)
@@ -206,7 +221,9 @@ class GuidedBackpropReLU(Function):
 
         positive_mask_1 = (input > 0).type_as(grad_output)
         positive_mask_2 = (grad_output > 0).type_as(grad_output)
-        grad_input = torch.addcmul(torch.zeros(input.size()).type_as(input), torch.addcmul(torch.zeros(input.size()).type_as(input), grad_output, positive_mask_1), positive_mask_2)
+        grad_input = torch.addcmul(torch.zeros(input.size()).type_as(input),
+                                   torch.addcmul(torch.zeros(input.size()).type_as(input), grad_output,
+                                                 positive_mask_1), positive_mask_2)
 
         return grad_input
 
@@ -222,9 +239,9 @@ class GuidedBackpropReLUModel:
         def recursive_relu_apply(module_top):
             for idx, module in module_top._modules.items():
                 recursive_relu_apply(module)
-                if module.__class__.__name__ == "ReLU":
+                if module.__class__.__name__ == 'ReLU':
                     module_top._modules[idx] = GuidedBackpropReLU.apply
-
+                
         # replace ReLU with GuidedBackpropReLU
         recursive_relu_apply(self.model)
 
@@ -260,107 +277,145 @@ class GuidedBackpropReLUModel:
 
 
 def get_model(opt):
-    print(opt.dataset)
-    if opt.dataset == "cifar10":
+        print(opt.dataset)
+        if(opt.dataset == 'cifar10'):
         # Model
-        netC = PreActResNet18().to(opt.device)
-        netG = UnetGenerator(opt).to(opt.device)
-    if opt.dataset == "gtsrb":
-        # Model
-        netC = PreActResNet18(num_classes=opt.num_classes).to(opt.device)
-        netG = UnetGenerator(opt).to(opt.device)
+            netC = PreActResNet18().to(opt.device)
+            netG = UnetGenerator(opt).to(opt.device)
+        else: 
+            raise Exception("Invalid dataset")
+        
+        # Load pretrained classifier
+        path_model = os.path.join(opt.checkpoints, '{}_clean'.format(opt.saving_prefix), opt.dataset, '{}_{}_clean.pth.tar'.format(opt.dataset, opt.saving_prefix))
+        state_dict = torch.load(path_model)
 
-    # Load pretrained classifier and generator
-    model_path = os.path.join(opt.checkpoints, "{}_clean".format(opt.saving_prefix), opt.dataset, "{}_{}_clean.pth.tar".format(opt.dataset, opt.saving_prefix))
-    state_dict = torch.load(model_path)
-    netG.load_state_dict(state_dict["netG"])
-    for param in netG.parameters():
-        param.requires_grad = False
-    netC.load_state_dict(state_dict["netC"])
-    for param in netC.parameters():
-        param.requires_grad = False
-    netC.eval()
+        netG.load_state_dict(state_dict['netG'])
+        for param in netG.parameters():
+            param.requires_grad = False
 
-    return netC, netG
-
-
-def show_cam_on_image(img, mask, idx, result_path, opt):
+        netC.load_state_dict(state_dict['netC'])
+        for param in netC.parameters():
+            param.requires_grad = False
+        netC.eval()
+        
+        return netC, netG
+    
+def get_clean_model(opt):
+        if(opt.dataset == 'cifar10'):
+            classifier = PreActResNet18()
+        else:
+            raise Exception("Invalid Dataset")
+        # Load pretrained classifier
+        clean_model_path = os.path.join(opt.checkpoints, opt.load_checkpoint_clean, opt.dataset, "{}_{}.pth.tar".format(opt.dataset, opt.load_checkpoint_clean))
+        state_dict = torch.load(clean_model_path)
+        classifier.load_state_dict(state_dict['netC'])
+        for param in classifier.parameters():
+            param.requires_grad = False
+        classifier.eval()
+        return classifier.to(opt.device)
+    
+def show_cam_on_image(img, mask, idx, result_path, opt, prefix=""):
     heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
     heatmap = np.float32(heatmap) / 255
-    cam = heatmap + np.float32(img) / 255
+    cam = heatmap + np.float32(img)/255
     cam = cam / np.max(cam)
-
+    
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(os.path.join(result_path, "bd{}.png".format(idx)), np.uint8(img))
-    cv2.imwrite(os.path.join(result_path, "cam{}.png".format(idx)), np.uint8(255 * cam))
+    cv2.imwrite(os.path.join(result_path, prefix + "bd{}.png".format(idx)), np.uint8(img))
+    cv2.imwrite(os.path.join(result_path, prefix + "cam{}.png".format(idx)), np.uint8(255 * cam))
     cv2.imwrite("heatmap.png", np.uint8(255 * heatmap))
-    heatmap = heatmap[:, :, ::-1].copy()
+    heatmap = heatmap[:,:,::-1].copy()
 
-    heatmap, img = torch.tensor(heatmap).permute(2, 0, 1), torch.tensor(img / 255.0).permute(2, 0, 1)
+    heatmap, img = torch.tensor(heatmap).permute(2, 0, 1), torch.tensor(img / 255.).permute(2, 0, 1)
     heatmap, img = F.interpolate(heatmap.unsqueeze(0), scale_factor=4), F.interpolate(img.unsqueeze(0), scale_factor=4)
     return heatmap[0], img[0]
-
-
+    
 def create_bd(inputs_clean, generator, opt):
+    gauss_smooth = T.GaussianBlur(kernel_size=opt.kernel_size, sigma=opt.sigma)
     noise_bd = generator(inputs_clean)
+    noise_bd = low_freq(noise_bd, opt)    
     inputs_bd = torch.clamp(inputs_clean + noise_bd * opt.noise_rate, -1, 1)
-    targets_bd = torch.ones(inputs.shape[0]).to(opt.device) * opt.target_label
-
+    inputs_bd = gauss_smooth(inputs_bd)
+    targets_bd = torch.ones(inputs_clean.shape[0]).to(opt.device) * opt.target_label
+    
     return inputs_bd, targets_bd
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     opt = get_arguments().parse_args()
-    if opt.dataset == "cifar10":
+    if(opt.dataset == 'cifar10'):
         opt.input_height = 32
         opt.input_width = 32
-        opt.input_channel = 3
-    elif opt.dataset == "gtsrb":
-        opt.input_height = 32
-        opt.input_width = 32
-        opt.input_channel = 3
-        opt.num_classes = 13
+        opt.input_channel  = 3 
     else:
         raise Exception("Invalid Dataset")
-
+    # args = get_args()
+    
     # Load pretrained model
     model, generator = get_model(opt)
+    model_clean = get_clean_model(opt)
     denormalizer = get_denormalize(opt)
-
+    
+    # Prepare dataset
     dl = get_dataloader(opt, False)
-
     it = iter(dl)
     inputs, targets = next(it)
     inputs, targets = inputs.to(opt.device), targets.to(opt.device)
 
     # Create backdoor input
-    inputs_bd, _ = create_bd(inputs[:10], generator, opt)
-    grad_cam = GradCam(model=model, feature_module=model.layer3, target_layer_names=["1"], use_cuda=True)
+    inputs_bd, _ = create_bd(inputs[:20], generator, opt)
+    print(inputs_bd.shape)
+    
+    grad_cam = GradCam(model=model, feature_module=model.layer3, \
+                       target_layer_names=["1"], use_cuda=True)
+    grad_cam_clean = GradCam(model=model_clean, feature_module=model_clean.layer3, \
+                       target_layer_names=["1"], use_cuda=True)
     bs = inputs_bd.shape[0]
     heatmaps = []
     imgs = []
     cams = []
-
-    for idx in range(10):
+    
+    #for sample_idx in range(bs):
+    for idx in range(20):
         input_single = inputs_bd[idx].unsqueeze(0).requires_grad_(True)
         print(input_single.shape)
-        if denormalizer:
+        if(denormalizer):
             img = denormalizer(input_single).squeeze(0)
         else:
             img = input_single.squeeze(0)
-
+            
         img = img.cpu().detach().numpy() * 255
-        img = img.transpose((1, 2, 0))
+        print(img.shape)
+        img = img.transpose((1, 2, 0)) 
+
+        result_dir = os.path.join(opt.results, opt.dataset)
+        if not os.path.exists(result_dir):
+            os.makedirs(result_dir)
 
         # If None, returns the map for the highest scoring category.
         # Otherwise, targets the requested index.
         target_index = None
         mask = grad_cam(input_single, target_index)
-        result_dir = os.path.join(opt.results, opt.dataset)
-        if not os.path.exists(result_dir):
-            os.makedirs(result_dir)
-
-        # Show heatmap on image and save
         heatmap, img = show_cam_on_image(img, mask, idx, result_dir, opt)
         heatmaps.append(heatmap)
         imgs.append(img)
+        
+
+        input_single = inputs[idx].unsqueeze(0).requires_grad_(True)
+        print(input_single.shape)
+        if(denormalizer):
+            img = denormalizer(input_single).squeeze(0)
+        else:
+            img = input_single.squeeze(0)
+            
+        img = img.cpu().detach().numpy() * 255
+        print(img.shape)
+        img = img.transpose((1, 2, 0)) 
+
+        # If None, returns the map for the highest scoring category.
+        # Otherwise, targets the requested index.
+        target_index = None
+        mask = grad_cam_clean(input_single, target_index)
+        result_dir = os.path.join(opt.results, opt.dataset)
+        if not os.path.exists(result_dir):
+            os.makedirs(result_dir)
+        heatmap, img = show_cam_on_image(img, mask, idx, result_dir, opt, prefix="clean")
